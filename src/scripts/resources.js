4d2672e19bea702f3da08ef3322ea829e43ba908
shihan98 <shihanthathsara98@gmail.com>
2023-01-26 12:02:30 +0530

    test

---
+++ src/scripts/resources.js
@@ -0,0 +1,429 @@
+import DatabaseOperations from './DBOperations';
+import Utils from './utils';
+
+class Resources {
+  static initialLoading() {
+    const rtnObj = {};
+    try {
+      // rtnObj.appUser = this.resolveAppUser();
+      rtnObj.scriptUrl = Utils.getScriptUrl();
+      rtnObj.appName = Utils.getAppName();
+      rtnObj.appDescription = Utils.getAppDescription();
+      rtnObj.appRedirectURL = Utils.getAppRedirectURL();
+      // rtnObj.facultiesDepartments = this.getFacultiesAndDepartmentsList();
+      rtnObj.posts = this.getPostData();
+      rtnObj.vacancies = this.getVacancyData();
+      rtnObj.faculties = this.getFacultyData();
+      rtnObj.departments = this.getDepartmentData();
+
+      rtnObj.bdTitles = this.getBdTitleData();
+      rtnObj.pgdTitles = this.getPgdTitleData();
+      rtnObj.subjectAreas = this.getSubjectAreaData();
+
+      rtnObj.reqMain = this.getReqMainData();
+      rtnObj.requirements = this.getRequirements();
+
+      return rtnObj;
+    } catch (error) {
+      console.error('Error occurred while saveLeave in Resources', error);
+      throw new Error(`Error occurred while initialLoading`);
+    }
+  }
+
+  static resolveAppUser() {
+    try {
+      const retObj = {};
+
+      if (Utils.getCurrentUser()) {
+        const email = Utils.getCurrentUser();
+
+        DatabaseOperations.cacheEnabled = false;
+        DatabaseOperations.initilizeDatabase(Utils.getMainDBID());
+        DatabaseOperations.openDatabaseConnection(Utils.getAppRoleSheetName());
+        // const foundObj = DatabaseOperations.queryDatabase(`KEY:Email === "${email}"`);
+        // const foundObj = DatabaseOperations.readDatabaseCache();
+
+        if (foundObj && typeof foundObj !== 'undefined') {
+          retObj.data = foundObj;
+          return retObj;
+        }
+        const facCode = Utils.getCurrentUser().split('@')[1].split('.')[0];
+        let faculty = '';
+        switch (facCode) {
+          case 'agri':
+            faculty = 'Faculty of Agriculture';
+            break;
+          case 'ahs':
+            faculty = 'Faculty of Allied Health Sciences';
+            break;
+          case 'arts':
+            faculty = 'Faculty of Arts';
+            break;
+          case 'dental':
+            faculty = 'Faculty of Dental Sciences';
+            break;
+          case 'eng':
+            faculty = 'Faculty of Engineering';
+            break;
+          case 'mgt':
+            faculty = 'Faculty of Management';
+            break;
+          case 'med':
+            faculty = 'Faculty of Medicine';
+            break;
+          case 'sci':
+            faculty = 'Faculty of Science';
+            break;
+          case 'vet':
+            faculty = 'Faculty of Veterinary Medicine and Animal Science';
+            break;
+          default:
+            faculty = 'UNIVERSITY OF PERADENIYA';
+            break;
+        }
+
+        const guestUser = {
+          Name: 'INTERNAL_USER',
+          Email: email,
+          Faculty: faculty,
+          Department: '',
+          Role: 'USER_UOP',
+        };
+
+        const outerArray = [];
+        outerArray.push(guestUser);
+        retObj.data = outerArray;
+
+        return retObj;
+      }
+      const guestUser = {
+        Name: 'Guest',
+        Email: '',
+        Faculty: '',
+        Department: '',
+        Role: 'GUEST',
+      };
+
+      const outerArray = [];
+      outerArray.push(guestUser);
+      retObj.data = outerArray;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while resolveAppUser in Resources', error);
+      throw new Error(`Error occurred while resolveAppUser`);
+    }
+  }
+
+  static getReqMainData() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getReqDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getReqMainSheetName());
+      // const foundObj = DatabaseOperations.queryDatabase(`KEY:STATUS === "OPEN"`);
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.reqMain = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getReqMainData in Resources', error);
+      throw new Error(`Error occurred while getReqMainData`);
+    }
+  }
+
+  static getRequirements() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getReqDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getReqSheetName());
+      // const foundObj = DatabaseOperations.queryDatabase(`KEY:STATUS === "OPEN"`);
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.requirements = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getReqData in Resources', error);
+      throw new Error(`Error occurred while getReqData`);
+    }
+  }
+
+  static getBdTitleData() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getMainDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getBdTitlesListSheetName());
+      // const foundObj = DatabaseOperations.queryDatabase(`KEY:STATUS === "OPEN"`);
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.bdTitles = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getBdTitleData in Resources', error);
+      throw new Error(`Error occurred while getBdTitleData`);
+    }
+  }
+
+  static getPgdTitleData() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getMainDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getPgdTitlesListSheetName());
+      // const foundObj = DatabaseOperations.queryDatabase(`KEY:STATUS === "OPEN"`);
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.pgdTitles = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getPgdTitleData in Resources', error);
+      throw new Error(`Error occurred while getPgdTitleData`);
+    }
+  }
+
+  static getSubjectAreaData() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getMainDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getSubjectAreaListSheetName());
+      // const foundObj = DatabaseOperations.queryDatabase(`KEY:STATUS === "OPEN"`);
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.subjectAreas = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getSubjectAreaData in Resources', error);
+      throw new Error(`Error occurred while getSubjectAreaData`);
+    }
+  }
+
+  static getVacancyData() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getDataDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getVacanciesListSheetName());
+      // const foundObj = DatabaseOperations.queryDatabase(`KEY:STATUS === "OPEN"`);
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.vacancies = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getVacancyData in Resources', error);
+      throw new Error(`Error occurred while getVacancyData`);
+    }
+  }
+
+  static getPostData() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getDataDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getPostListSheetName());
+      // const foundObj = DatabaseOperations.queryDatabase(`KEY:POST !== "POST"`);
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.posts = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getPostData in Resources', error);
+      throw new Error(`Error occurred while getPostData`);
+    }
+  }
+
+  static getFacultyData() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getDataDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getFacListSheetName());
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.faculties = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getFacultyData in Resources', error);
+      throw new Error(`Error occurred while getFacultyData`);
+    }
+  }
+
+  static getDepartmentData() {
+    try {
+      DatabaseOperations.cacheEnabled = false;
+      DatabaseOperations.initilizeDatabase(Utils.getDataDBID());
+      DatabaseOperations.openDatabaseConnection(Utils.getDeptListSheetName());
+      const foundObj = DatabaseOperations.readDatabaseCache();
+
+      const retObj = {};
+
+      retObj.departments = foundObj;
+
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while getDepartmentData in Resources', error);
+      throw new Error(`Error occurred while getDepartmentData`);
+    }
+  }
+
+  /*
+  static saveRequest(obj) {
+    try {
+      if (obj) {
+        DatabaseOperations.cacheEnabled = false;
+        DatabaseOperations.initilizeDatabase(Utils.getMainDBID());
+        DatabaseOperations.openDatabaseConnection(Utils.getApplicationSheetName());
+
+        const currentDate = Utils.getCurrentDate();
+        const applicationID = Utils.genApplicationID();
+
+        const request = {
+          // id: '=ROW()',
+          ApplicationID: applicationID,
+          SubmittedDate: currentDate,
+
+          VacancyID: obj.vacancyID,
+          VacancyPost: obj.vacancyPost,
+          VacancyFaculty: obj.vacancyFac,
+          VacancyDepartment: obj.vacancyDept,
+
+          Gender: obj.pdGender,
+          Title: obj.personalTitle,
+          NameWithInitials: obj.personalNameInit,
+          FullName: obj.personalFullName,
+          DateOfBirth: obj.personalDOB,
+          PostalAddress: obj.personalAddress,
+          CivilStatus: obj.civilStatus,
+          Mobile: obj.personalMobile,
+          Recidence: obj.personalTp,
+          Email: obj.personalEmail,
+          District: obj.district,
+          Electorate: obj.electorate,
+          Province: obj.province,
+          City: obj.city,
+          Citizenship: obj.Citizenship,
+          DescentOrByRegistration: obj.descentOrReg,
+          NIC: obj.nic,
+          CountryOfCitizenship: obj.specifyCountry,
+          PassportNo: obj.passportNo,
+          SpouseName: obj.spouseName,
+          SpouseDesignation: obj.spouseDesignation,
+          HighestEducation: obj.highestEducation,
+          ProficiencyOnLanguages: obj.lang,
+
+          BasicDegree: obj.basicDegree,
+          BasicDegreeCountry: obj.bdCountry,
+          BasicDegreeUniversity: obj.bdUniversity,
+          BasicDegreeYearFrom: obj.bdYearFrom,
+          BasicDegreeYearTo: obj.bdYearTo,
+          BasicDegreeClass: obj.bdlass,
+          BasicDegreeGPA: obj.bdGPA,
+
+          PGDegree: obj.pgd,
+
+          Awards: obj.awards,
+
+          ResearchPublicationsBooks: obj.books,
+          ResearchPublicationsJournals: obj.journals,
+          ResearchPublicationsAbstracts: obj.abstracts,
+
+          CommendationsAndPunishments: obj.commendations,
+
+          Vacations: obj.vacations,
+
+          ExtraCurricularActivities: obj.extraCurrActivity,
+
+          RefereeName1: obj.rName1,
+          RefereeTelephone1: obj.rTelephone1,
+          RefereeAddress1: obj.rAddress1,
+          RefereeEmail1: obj.rEmail1,
+
+          RefereeName2: obj.rName2,
+          RefereeTelephone2: obj.rTelephone2,
+          RefereeAddress2: obj.rAddress2,
+          RefereeEmail2: obj.rEmail2,
+
+          Designation: obj.poDesignation,
+          InstitutionOrDepartment: obj.poDept,
+          PresentOccupationFrom: obj.poFrom,
+          SalaryDrawn: obj.poSalaryDrawn,
+
+          PreviousEmployments: obj.pEmployments,
+          BondViolation: bondViolator,
+          BondValue: bondValue,
+          BondUniversityOrInstitute: uniInstitute,
+        };
+
+        const funcStatus = DatabaseOperations.saveItem(request);
+
+        if (!funcStatus) {
+          console.error(`Error occurred while saveRequest in Resources`);
+        }
+
+        // Send Mail to Submitter
+        // const heading = 'ONLINE APPLICATION - University of Peradeniya';
+        // const description = 'Your application has been submitted successfully..!';
+        // const name = obj.FullName;
+        // const email = obj.Email;
+        // const post = obj.Post;
+        // const { VacancyID } = obj;
+        // const faculty = obj.Faculty;
+        // const department = obj.Department;
+        // const emailAddress = `${obj.Email},${Utils.getProcessingEmails()}`;
+
+        // Utils.sendMail(heading, description, emailAddress, name, email, post, VacancyID, faculty, department);
+
+        return funcStatus;
+      }
+
+      return '';
+    } catch (error) {
+      console.error('Error occurred while saveRequest in Resources', error);
+      throw new Error(`Error occurred while saveRequest`);
+    }
+    DatabaseOperations.cacheEnabled = false;
+    DatabaseOperations.initilizeDatabase(Utils.getMainDBID());
+    DatabaseOperations.openDatabaseConnection(Utils.getApplicationSheetName());
+    const request = obj;
+    request.status = true;
+    request.connectedDatabase = DatabaseOperations.saveItem(obj);
+    return request;
+  }
+  */
+
+  static saveRequest(obj) {
+    let retObj = '';
+    try {
+      if (obj) {
+        DatabaseOperations.cacheEnabled = false;
+        DatabaseOperations.initilizeDatabase(Utils.getMainDBID());
+        DatabaseOperations.openDatabaseConnection(Utils.getApplicationSheetName());
+        retObj = DatabaseOperations.saveItem(obj);
+      }
+      return retObj;
+    } catch (error) {
+      console.error('Error occurred while saveRequest in Resources', error);
+      throw new Error(`Error occurred while saveRequest`);
+    }
+  }
+}
+
+export default Resources;
---
+++ src/scripts/utils.js
@@ -0,0 +1,236 @@
+const ERROR_EMAIL = 'youremail@sci.pdn.ac.lk';
+const TIME_ZONE = 'Asia/Colombo';
+const DATE_FORMAT = 'MM/dd/yyyy HH:mm:ss';
+const UUID_FORMAT = 'MMddyyyyHHmmss';
+const APP_NAME = 'ONLINE APPLICATION';
+const APP_DESCRIPTION = 'Academic Establishment. University of Peradeniya.';
+const APP_URL =
+  'https://script.google.com/macros/s/AKfycbwftRGdFiH_wwlNlEl5teYLr_isAKauK-OskdggQR_7VsAINLaQaRZ6NUqIx_0o-YwR8A/exec';
+const ALIASMAIL = 'portal@gs.pdn.ac.lk';
+const UUID_CODE = 'OA';
+
+const DBID = '1MTE8MdB-BwDG80wFlNI8zaADsnkG3XJsHxLW79ndXa4';
+const DATA_DBID = '1QdubFe5pbNQyevoK_LQS021OPrYxve_aGuD9xGEUI4o';
+const REQ_DBID = '1T5GMDaXjgyF0QMbsznDW9jWpap64VY1hs-FJ8uQEYk8';
+
+const APP_ROLE_SHEET = 'roles';
+const VACANCY_APPLICATION_SHEET = 'mainsheet';
+const VACANCY_LIST_SHEET = 'vacancy';
+const POST_LIST_SHEET = 'post';
+const FAC_LIST_SHEET = 'faculty';
+const DEPT_LIST_SHEET = 'department';
+
+const BASIC_DEGREE_LIST_SHEET = 'bdTitle';
+const POSTGRAD_DEGREE_LIST_SHEET = 'pgdTitle';
+const SUBJECT_AREA_LIST_SHEET = 'subjectArea';
+
+const REQUIREMENTS_MAINSHEET = 'mainsheet';
+const REQUIREMENTS_LIST_SHEET = 'requirements';
+
+const DBID_FAC_DEPT = '1MTE8MdB-BwDG80wFlNI8zaADsnkG3XJsHxLW79ndXa4';
+const SHEET_FAC_DEPT = 'RolesAccount';
+
+const PROCESSING_EMAILS = 'portal@gs.pdn.ac.lk';
+
+let cKey = null;
+let chunks = [];
+
+class Utils {
+  static ChunkyCache(cache, chunkSize) {
+    return {
+      put(key, value, timeout) {
+        const json = JSON.stringify(value);
+        const cSize = Math.floor(chunkSize / 2);
+        let index = 0;
+        while (index < json.length) {
+          cKey = `${key}_${index}`;
+          chunks.push(cKey);
+          cache.put(cKey, json.substr(index, cSize), timeout + 5);
+          index += cSize;
+        }
+
+        const superBlk = {
+          chunkSize,
+          chunks,
+          length: json.length,
+        };
+        cache.put(key, JSON.stringify(superBlk), timeout);
+      },
+      get(key) {
+        const superBlkCache = cache.get(key);
+        if (superBlkCache != null) {
+          const superBlk = JSON.parse(superBlkCache);
+          chunks = superBlk.chunks.map(function (cKey2) {
+            return cache.get(cKey2);
+          });
+          if (
+            chunks.every(function (c) {
+              return c != null;
+            })
+          ) {
+            return JSON.parse(chunks.join(''));
+          }
+        }
+        return null;
+      },
+    };
+  }
+
+  static getDateFormat() {
+    return DATE_FORMAT;
+  }
+
+  static getCurrentDate() {
+    const date = Utilities.formatDate(new Date(), TIME_ZONE, DATE_FORMAT);
+    return date;
+  }
+
+  static generateUUID() {
+    const date = Utilities.formatDate(new Date(), TIME_ZONE, UUID_FORMAT);
+    return `${UUID_CODE}_${date}`;
+  }
+
+  static parseDate(dateValue) {
+    const date = Utilities.formatDate(new Date(dateValue), TIME_ZONE, DATE_FORMAT);
+    return date;
+  }
+
+  static getCurrentUser() {
+    return Session.getActiveUser().getEmail();
+  }
+
+  static getAppName() {
+    return APP_NAME;
+  }
+
+  static getAppDescription() {
+    return APP_DESCRIPTION;
+  }
+
+  static getAppRedirectURL() {
+    return APP_URL;
+  }
+
+  static getScriptUrl() {
+    const url = ScriptApp.getService().getUrl();
+    return url;
+  }
+
+  static getALIASMAIL() {
+    return ALIASMAIL;
+  }
+
+  static getMainDBID() {
+    return DBID;
+  }
+
+  static getReqDBID() {
+    return REQ_DBID;
+  }
+
+  static getReqMainSheet() {
+    return REQUIREMENTS_MAINSHEET;
+  }
+
+  static getReqSheetName() {
+    return REQUIREMENTS_LIST_SHEET;
+  }
+
+  static getAppRoleSheetName() {
+    return APP_ROLE_SHEET;
+  }
+
+  static getDataDBID() {
+    return DATA_DBID;
+  }
+
+  // static getVacanciesDBID() {
+  //   return VACANCY_DBID;
+  // }
+
+  static getBdTitlesListSheetName() {
+    return BASIC_DEGREE_LIST_SHEET;
+  }
+
+  static getPgdTitlesListSheetName() {
+    return POSTGRAD_DEGREE_LIST_SHEET;
+  }
+
+  static getSubjectAreaListSheetName() {
+    return SUBJECT_AREA_LIST_SHEET;
+  }
+
+  static getApplicationSheetName() {
+    return VACANCY_APPLICATION_SHEET;
+  }
+
+  static getVacanciesListSheetName() {
+    return VACANCY_LIST_SHEET;
+  }
+
+  static getPostListSheetName() {
+    return POST_LIST_SHEET;
+  }
+
+  static getFacListSheetName() {
+    return FAC_LIST_SHEET;
+  }
+
+  static getDeptListSheetName() {
+    return DEPT_LIST_SHEET;
+  }
+
+  static getFacultyDepartmentDBID() {
+    return DBID_FAC_DEPT;
+  }
+
+  static getFacultyDepartmentSheetName() {
+    return SHEET_FAC_DEPT;
+  }
+
+  static getProcessingEmails() {
+    return PROCESSING_EMAILS;
+  }
+
+  static sendMail(heading, description, emailAddress, name, email, post, VacancyID, faculty, department) {
+    try {
+      const htmlOutput = HtmlService.createHtmlOutputFromFile('email_template');
+      let message = htmlOutput.getContent();
+      message = message.replace('%heading', heading);
+      message = message.replace('%description', description);
+
+      message = message.replace('%param1', name);
+      message = message.replace('%param2', email);
+      message = message.replace('%param3', post);
+      message = message.replace('%param4', VacancyID);
+      message = message.replace('%param5', `${department} - ${faculty}`);
+      message = message.replace('%appURL', APP_URL);
+
+      const subject = 'Online Application -  University of Peradeniya';
+      GmailApp.sendEmail(emailAddress, subject, '', {
+        from: ALIASMAIL,
+        replyTo: ALIASMAIL,
+        name: 'University of Peradeniya',
+        htmlBody: message,
+      });
+    } catch (e) {
+      console.error('Error ocuured while Send Mail in Utils', e);
+      throw new Error(`Error ocuured while Send Mail in Utils`);
+    }
+  }
+
+  static errHandler(e, strFunc) {
+    let message = `${e.message}\n in file: ${e.fileName} on line: ${e.lineNumber}`;
+    const subject = `${APP_NAME} ERROR OCCURED | FOS APPS |  ${strFunc}`;
+    const errProps = JSON.stringify(this.onError);
+    message = `${subject}\n${message}\n onError: ${errProps}`;
+    GmailApp.sendEmail(ERROR_EMAIL, subject, message);
+  }
+
+  static genApplicationID() {
+    const DB = SpreadsheetApp.openById(this.getMainDBID());
+    const APPLICATION_ID = DB.getSheetByName(this.getApplicationSheetName()).getLastRow();
+    return APPLICATION_ID;
+  }
+}
+export default Utils;
