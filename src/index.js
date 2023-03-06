baf516a0ab9846d1394fe4466ac43bdd56f153d1
shihan98 <shihanthathsara98@gmail.com>
2023-03-06 16:28:51 +0530

    4 new pages added

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import Resources from './scripts/resources';
 import Utils from './scripts/utils';
 
 function doGet(e) {
+  let htmlOutput = '';
   if (!e.parameter.page) {
     // When no specific page requested, return "home page" Ex : ?page=hod
-    return HtmlService.createTemplateFromFile('index_view')
+    htmlOutput = HtmlService.createTemplateFromFile('report_shortlist');
+    return htmlOutput
       .evaluate()
       .setTitle(Utils.getAppName())
       .addMetaTag('viewport', 'width=device-width, initial-scale=1')
       .setXFrameOptionsMode(HtmlService.XFrameOptionsMode.ALLOWALL);
   }
   // else, use page parameter to pick an html file from the script
-  return HtmlService.createTemplateFromFile(e.parameter.page)
+
+  htmlOutput = HtmlService.createTemplateFromFile(e.parameter.page);
+  return htmlOutput
     .evaluate()
     .setTitle(Utils.getAppName())
     .addMetaTag('viewport', 'width=device-width, initial-scale=1')
@@ -30,12 +34,32 @@ function saveRequest(obj) {
   return JSON.stringify(Resources.saveRequest(obj));
 }
 
-function update(obj) {
-  return JSON.stringify(Resources.updateRequest(obj));
+function updateApplicationStatus(obj) {
+  return JSON.stringify(Resources.updateApplicationStatus(obj));
 }
 
 function initialShortlistAppLoading() {
   return JSON.stringify(Resources.initialShortlistAppLoading());
+}
+
+function getApplications() {
+  return JSON.stringify(Resources.getApplications());
+}
+
+function getScriptUrl() {
+  return Utils.getScriptUrl();
+}
+
+let tempData = '';
+
+function getTempData() {
+  tempData = PropertiesService.getScriptProperties().getProperty('tempData');
+  return tempData;
+}
+
+function setTempData(obj) {
+  tempData = JSON.stringify(obj);
+  PropertiesService.getScriptProperties().setProperty('tempData', tempData);
 }
 
 global.doGet = doGet;
@@ -43,3 +67,8 @@ global.include = include;
 global.initialLoading = initialLoading;
 global.saveRequest = saveRequest;
 global.initialShortlistAppLoading = initialShortlistAppLoading;
+global.updateApplicationStatus = updateApplicationStatus;
+global.getApplications = getApplications;
+global.getScriptUrl = getScriptUrl;
+global.getTempData = getTempData;
+global.setTempData = setTempData;
